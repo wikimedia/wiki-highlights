94af4f56e69a10abf0da4fd85584f845c71e3fe6
eamedina <eamedina@wikimedia.org>
2023-09-15 15:49:22 -0400

    Add WMF linter tests

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
-<script setup>
-import HighlightCard from '../components/HighlightCard.vue'
-import data from '../data.json'
-</script>
-
 <template>
-  <main>
-    <h1>Wiki highlights</h1>
-    <div v-for="content in data.content" :key="content.title" >
-      <HighlightCard :content="content" />
-    </div>
-  </main>
+	<main>
+		<h1>Wiki highlights</h1>
+		<div v-for="content in data.content" :key="content.title">
+			<HighlightCard :content="content" />
+		</div>
+	</main>
 </template>
+
+<script setup>
+import HighlightCard from '../components/HighlightCard.vue';
+import data from '../data.json';
+</script>
