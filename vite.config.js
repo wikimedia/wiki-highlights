6053508556b8b166fd8e808e319ef1480c1ae9ec
eamedina <eamedina@wikimedia.org>
2023-09-15 11:54:57 -0400

    Try with base

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  // base: '/wiki-highlights/',
+  base: '/wiki-highlights/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
