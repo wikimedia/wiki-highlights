af4e1e85b0fcc920ea9cd092f922312d1bc2084f
Stephane Bisson <sbisson@wikimedia.org>
2023-09-26 14:40:09 -0400

    Categorized home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,26 @@
 <template>
 	<main>
 		<h1>Wiki highlights</h1>
-		<div v-for="article in data.articles" :key="article.title">
-			<span>{{ article.category }}</span>&nbsp;&mdash;&nbsp;
-			<a :href="'#/highlights/' + article.title">{{ article.title }}</a>
+		<div v-for="( articles, category ) in categories">
+			<h2>{{ category }}</h2>
+			<div v-for="article in articles" :key="article.title">
+				<span>{{ article.title }}</span>
+				&nbsp;(
+				<a :href="'#/highlights/' + article.title">{{ article.highlights.length }} highlights</a>
+				)
+			</div>
 		</div>
+
 	</main>
 </template>
 
 <script setup>
 import data from '../data.json';
+var groupBy = function(xs, key) {
+  return xs.reduce(function(rv, x) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {});
+};
+var categories = groupBy( data.articles, 'category' );
 </script>
