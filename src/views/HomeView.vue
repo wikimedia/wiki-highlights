46426aa43ddbb382f269ab83743122d8c9d310ec
Stephane Bisson <sbisson@wikimedia.org>
2023-09-26 15:14:33 -0400

    Fix linting errors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,27 @@
 <template>
 	<main>
 		<h1>Wiki highlights</h1>
-		<div v-for="( articles, category ) in categories">
+		<div v-for="( articles, category ) in categories" :key="category">
 			<h2>{{ category }}</h2>
 			<div v-for="article in articles" :key="article.title">
 				<span>{{ article.title }}</span>
 				&nbsp;(
-				<a :href="'#/highlights/' + article.title">{{ article.highlights.length }} highlights</a>
+				<a :href="'#/highlights/' + article.title">
+					{{ article.highlights.length }} highlights
+				</a>
 				)
 			</div>
 		</div>
-
 	</main>
 </template>
 
 <script setup>
 import data from '../data.json';
-var groupBy = function(xs, key) {
-  return xs.reduce(function(rv, x) {
-    (rv[x[key]] = rv[x[key]] || []).push(x);
-    return rv;
-  }, {});
+const groupBy = function ( xs, key ) {
+	return xs.reduce( function ( rv, x ) {
+		( rv[ x[ key ] ] = rv[ x[ key ] ] || [] ).push( x );
+		return rv;
+	}, {} );
 };
-var categories = groupBy( data.articles, 'category' );
+const categories = groupBy( data.articles, 'category' );
 </script>
