73e4f9a92b0f78bbd133635c520f6d54a1d00547
Stephane Bisson <sbisson@wikimedia.org>
2023-09-27 16:05:37 -0400

    snap scroll

--- src/views/HighlightView.vue
+++ src/views/HighlightView.vue
@@ -1,11 +1,18 @@
 <template>
 	<main class="wiki-highlight-view">
-		<div v-for="( highlight, index ) in article.highlights" :key="highlight.image">
+		<div
+			v-for="( highlight, index ) in article.highlights"
+			:key="highlight.image"
+			class="wiki-highlight-view-card"
+		>
 			<HighlightCard
 				:image="highlight.image"
 				:title="index === 0 ? article.title : ''"
 				:text="highlight.text"
 			/>
+		</div>
+		<div class="wiki-highlight-view-progressbar">
+			(progress bar)
 		</div>
 	</main>
 </template>
@@ -25,7 +32,23 @@ const article = data.articles.find( ( a ) => a.title === props.title );
 </script>
 
 <style>
-.wiki-highlight-view > div {
+.wiki-highlight-view {
 	height: 100vh;
+	overflow-y: scroll;
+	scroll-snap-type: y mandatory;
+}
+
+.wiki-highlight-view > .wiki-highlight-view-card {
+	height: 100vh;
+	padding-bottom: 50px;
+	scroll-snap-stop: always;
+	scroll-snap-align: start;
+}
+
+.wiki-highlight-view-progressbar {
+	position: fixed;
+	width: 50%;
+	background-color: blue;
+	bottom: 0;
 }
 </style>
