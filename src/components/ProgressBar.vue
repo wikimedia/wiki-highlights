f45910b852d3c50cfcfdb11a93c98fd653784e56
Stephane Bisson <sbisson@wikimedia.org>
2023-09-28 10:42:12 -0400

    Progress bar component

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="wiki-highlight-progressbar-outer">
+		<div
+			:style="{ width: progress + '%' }"
+			class="wiki-highlight-progressbar-inner"
+		/>
+	</div>
+</template>
+
+<script setup>
+defineProps( {
+	progress: { type: Number, required: true }
+} );
+</script>
+
+<style>
+.wiki-highlight-progressbar-outer {
+	background-color: #808080;
+	height: 10px;
+}
+
+.wiki-highlight-progressbar-inner {
+	background-color: #00f;
+	height: 10px;
+}
+</style>
