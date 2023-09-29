35745b4b409f6271a8170170c0ed3573f93831b8
Stephane Bisson <sbisson@wikimedia.org>
2023-09-29 13:05:00 -0400

    Randomize content

--- src/data.js
+++ src/data.js
@@ -1,11 +1,20 @@
 import data from './data.json';
 
-const groupBy = function ( xs, key ) {
+function shuffleArray ( array ) {
+    for ( let i = array.length - 1; i > 0; i-- ) {
+        const j = Math.floor( Math.random() * ( i + 1 ) );
+        [ array[i], array[j] ] = [ array[j], array[i] ];
+    }
+}
+
+function groupBy ( xs, key ) {
 	return xs.reduce( function ( rv, x ) {
 		( rv[ x[ key ] ] = rv[ x[ key ] ] || [] ).push( x );
 		return rv;
 	}, {} );
-};
+}
+
+shuffleArray( data.articles );
 const categories = groupBy( data.articles, 'category' );
 
 const getArticle = function ( title ) {
