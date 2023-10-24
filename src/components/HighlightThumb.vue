ed374d49fb52c6ee6dd2f5b8bb05c665dfca9d02
Huei Tan <huei90@gmail.com>
2023-10-24 14:40:45 +0200

    update

--- src/components/HighlightThumb.vue
+++ src/components/HighlightThumb.vue
@@ -77,8 +77,8 @@ function goToHighlight() {
 }
 
 .wiki-highlight-thumb-text p {
-	font-family:
- -apple-system,BlinkMacSystemFont,'Segoe UI',"Roboto","Lato",'Helvetica','Arial',sans-serif;
+	font-family: 
+ -apple-system,BlinkMacSystemFont,'Segoe UI','Roboto','Lato','Helvetica','Arial',sans-serif;
 	font-size: 1.1em;
 	color: #54595d;
 }
