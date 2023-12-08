ae9b53347c1c6e0f3b28743a39151cd88d827ce1
eamedina <eamedina@wikimedia.org>
2023-12-08 12:14:33 -0400

    Styling updates

--- src/components/HighlightThumb.vue
+++ src/components/HighlightThumb.vue
@@ -93,7 +93,7 @@ function goToHighlight() {
 
 .wiki-highlight-thumb-text p {
 	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Lato', 'Helvetica', 'Arial', sans-serif;
-	font-size: 1.1em;
+	font-size: 1em;
 	color: #54595d;
 }
 
